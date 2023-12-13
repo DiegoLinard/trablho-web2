200f2fe3db6516175daddee325b84119db5cf616
DiegoLinard <124385542+DiegoLinard@users.noreply.github.com>
2023-12-13 17:32:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,31 +6,30 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-// Conectar ao MongoDB (substitua 'meu-banco-de-dados' pelo nome do seu banco de dados)
+
 mongoose.connect('mongodb://localhost:27017/meu-banco-de-dados', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// Definir o modelo do cliente
+
 const Cliente = mongoose.model('Cliente', {
   nome: String,
   email: String,
 });
 
-// Rota para registrar um novo cliente usando o método POST
+
 app.post('/registrarCliente', async (req, res) => {
   const { nome, email } = req.body;
 
   try {
-    // Verificar se o cliente já existe no banco de dados
+  
     const clienteExistente = await Cliente.findOne({ email });
 
     if (clienteExistente) {
       return res.json({ success: false, message: 'Cliente já registrado' });
     }
 
-    // Criar um novo cliente
     const novoCliente = new Cliente({
       nome,
       email,
